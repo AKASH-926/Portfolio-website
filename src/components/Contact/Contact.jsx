5d650df1bb7d8acf42942399370819f4c0e7dac9
AKASH <akash.arthanur26@gmail.com>
2023-01-05 20:44:38 +0530

    added social  links

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -34,11 +34,11 @@ export const Contact = () => {
                         </div>
                         <div className="c-info-item">
                             <img src={linkedin} alt="" className="c-icon" />
-                            www.linkedin.com/in/akash-ap-61126b14a
+                            <a href="www.linkedin.com/in/akash-ap-61126b14a"> www.linkedin.com/in/akash-ap-61126b14a</a>
                         </div>
                         <div className="c-info-item">
                             <img src={github} alt="" className="c-icon" />
-                            https://github.com/AKASH-926
+                            <a href="https://github.com/AKASH-926">https://github.com/AKASH-926</a>
                         </div>
                         <div className="c-info-item">
                             <img src={location} alt="" className="c-icon" />
